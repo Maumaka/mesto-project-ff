1fd74504cf150a1cf1b21125b6d093d33a4b5d34
Kolya Ivanov <n.ivanov1984@yandex.ru>
2025-02-18 21:45:37 +0300

    first review

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,31 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardTemplate = () => {
+  return document.querySelector('#card-template').content.querySelector('.card').cloneNode(true);
+};
+
+const placesContainer = document.querySelector('.places__list');
+
+const insertCards = (cardElement) => {
+  const cardTemplates = cardTemplate();
+  cardTemplates.querySelector('.card__image').src = cardElement.link;
+  cardTemplates.querySelector('.card__image').alt = 'фото местности ' + cardElement.name;
+  cardTemplates.querySelector('.card__title').textContent = cardElement.name;
+  const deleteButton = cardTemplates.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', () => deleteCard(cardTemplates));
+  return cardTemplates;
+};
+
+const deleteCard = (deleteItem) => deleteItem.remove(); 
+
+initialCards.forEach(function(item) {
+  placesContainer.append(insertCards(item));
+});
+
+let element = document.querySelector('.my-element');
+
+element.addEventListener('click', function () {
+  console.log('Мы кликнули по элементу');
+}); 
